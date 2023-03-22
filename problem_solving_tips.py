9ead904b74ec817c98c565550ad7c1ab3218052c
sam88mohd <sam88mohd@gmail.com>
2023-03-22 23:32:52 +0800

    add find logs

--- problem_solving_tips.py
+++ problem_solving_tips.py
@@ -29,3 +29,18 @@ where to find log:
         Library/Logs
     windows:
         Event Viewer
+        
+1.creating a reproduction case
+2. finding root cause
+
+test our hypotesis in test envionment - include set up new test server, test data and so on
+
+iotop - use to check input output process.
+iostat , vmstat - show statistic on the io operation
+ionice - use to configure io resources
+iftop - show current traffic process
+
+rsync - backup software - us bwlimit to limit bw used
+Trickle - set bw limit
+
+nice - reduce priority of process using CPU
